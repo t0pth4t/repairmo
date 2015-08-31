2de64c5415d23a2a06d1a5ee7d8defbfca5b6f1e
t0pth4t <t0pth4t@yahoo.com>
2015-08-30 22:08:56 -0500

    init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('repairmoApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
