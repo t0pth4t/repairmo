ebb973c6efe32f429688ad0c7a380ac785515f71
t0pth4t <t0pth4t@yahoo.com>
2015-08-31 19:04:40 -0500

    fonts and image height

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -4,10 +4,10 @@ angular.module('repairmoApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
 
     $(window).scroll(function() {
-      if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+      if ($('.navbar').offset().top > 50) {
+        $('.navbar-fixed-top').addClass('top-nav-collapse');
       } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        $('.navbar-fixed-top').removeClass('top-nav-collapse');
       }
     });
 
